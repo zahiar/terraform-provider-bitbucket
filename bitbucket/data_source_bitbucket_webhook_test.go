cd0d8eb1102ec46c344371050563da15fb10fc6b
Zahiar Ahmed <me@zahiar.com>
2021-09-14 03:19:25 +0100

    GH-12 Add acceptance test for webhook data source

---
+++ bitbucket/data_source_bitbucket_webhook_test.go
@@ -0,0 +1,74 @@
+package bitbucket
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccBitbucketWebhookDataSource_basic(t *testing.T) {
+	workspaceSlug := os.Getenv("BITBUCKET_USERNAME")
+	projectName := "tf-acc-test-" + acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
+	projectKey := strings.ToUpper(acctest.RandStringFromCharSet(3, acctest.CharSetAlpha))
+	repoName := "tf-acc-test-" + acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
+	webhookDescription := "TF ACC Test Webhook"
+	webhookUrl := "https://example.com"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+				data "bitbucket_workspace" "testacc" {
+  					id = "%s"
+				}
+
+				resource "bitbucket_project" "testacc" {
+				  workspace  = data.bitbucket_workspace.testacc.id
+				  name       = "%s"
+				  key        = "%s"
+				  is_private = true
+				}
+
+				resource "bitbucket_repository" "testacc" {
+				  workspace   = data.bitbucket_workspace.testacc.id
+                  project_key = bitbucket_project.testacc.key
+				  name        = "%s"
+				}
+
+				resource "bitbucket_webhook" "testacc" {
+				  workspace   = data.bitbucket_workspace.testacc.id
+				  repository  = bitbucket_repository.testacc.name
+				  description = "%s"
+				  url         = "%s"
+				  events      = ["pullrequest:approved", "pullrequest:unapproved"]
+				  is_active   = true
+				}
+
+				data "bitbucket_webhook" "testacc" {
+				  id         = bitbucket_webhook.testacc.id
+				  workspace  = data.bitbucket_workspace.testacc.id
+				  repository = bitbucket_repository.testacc.name
+				}`, workspaceSlug, projectName, projectKey, repoName, webhookDescription, webhookUrl),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.bitbucket_webhook.testacc", "workspace", workspaceSlug),
+					resource.TestCheckResourceAttr("data.bitbucket_webhook.testacc", "repository", repoName),
+					resource.TestCheckResourceAttr("data.bitbucket_webhook.testacc", "description", webhookDescription),
+					resource.TestCheckResourceAttr("data.bitbucket_webhook.testacc", "url", webhookUrl),
+					resource.TestCheckResourceAttr("data.bitbucket_webhook.testacc", "is_active", "true"),
+
+					resource.TestCheckResourceAttr("data.bitbucket_webhook.testacc", "events.#", "2"),
+					resource.TestCheckResourceAttr("data.bitbucket_webhook.testacc", "events.0", "pullrequest:approved"),
+					resource.TestCheckResourceAttr("data.bitbucket_webhook.testacc", "events.1", "pullrequest:unapproved"),
+
+					resource.TestCheckResourceAttrSet("data.bitbucket_webhook.testacc", "id"),
+				),
+			},
+		},
+	})
+}
