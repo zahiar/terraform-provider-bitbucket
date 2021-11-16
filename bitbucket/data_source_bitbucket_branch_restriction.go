c301a7bb6e85915a8bcb074d4f1a6fcc2bec016e
Zahiar Ahmed <310030+zahiar@users.noreply.github.com>
2021-11-16 01:11:57 +0000

    GH-30 Add branch restriction resource & data source (#38)
    Along with docs.

---
+++ bitbucket/data_source_bitbucket_branch_restriction.go
@@ -0,0 +1,51 @@
+package bitbucket
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceBitbucketBranchRestriction() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceBitbucketBranchRestrictionRead,
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Description: "The ID of the branch restriction.",
+				Type:        schema.TypeString,
+				Required:    true,
+			},
+			"workspace": {
+				Description: "The slug or UUID (including the enclosing `{}`) of the workspace.",
+				Type:        schema.TypeString,
+				Required:    true,
+			},
+			"repository": {
+				Description:      "The name of the repository (must consist of only lowercase ASCII letters, numbers, underscores and hyphens).",
+				Type:             schema.TypeString,
+				Required:         true,
+				ValidateDiagFunc: validateRepositoryName,
+			},
+			"pattern": {
+				Description: "The pattern to match against branches this restriction will apply to.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"kind": {
+				Description: "The type of restriction to apply.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"value": {
+				Description: "A configurable value used by the following restrictions: `require_passing_builds_to_merge` uses it to define the number of minimum number of passing builds, `require_approvals_to_merge` uses it to define the minimum number of approvals before the PR can be merged, `require_default_reviewer_approvals_to_merge` uses it to define the minimum number of approvals from default reviewers before the PR can be merged.",
+				Type:        schema.TypeInt,
+				Computed:    true,
+			},
+		},
+	}
+}
+
+func dataSourceBitbucketBranchRestrictionRead(ctx context.Context, resourceData *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	return resourceBitbucketBranchRestrictionRead(ctx, resourceData, meta)
+}
---
+++ bitbucket/data_source_bitbucket_branch_restriction_test.go
@@ -0,0 +1,132 @@
+package bitbucket
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccBitbucketBranchRestrictionDataSource_basic(t *testing.T) {
+	workspaceSlug := os.Getenv("BITBUCKET_USERNAME")
+	projectName := "tf-acc-test-" + acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
+	projectKey := strings.ToUpper(acctest.RandStringFromCharSet(3, acctest.CharSetAlpha))
+	repoName := "tf-acc-test-" + acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
+	branchRestrictionPattern := "master"
+	branchRestrictionKind := "require_tasks_to_be_completed"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+					data "bitbucket_workspace" "testacc" {
+						id = "%s"
+					}
+	
+					resource "bitbucket_project" "testacc" {
+					  workspace = data.bitbucket_workspace.testacc.id
+					  name      = "%s"
+					  key       = "%s"
+					}
+	
+					resource "bitbucket_repository" "testacc" {
+					  workspace   = data.bitbucket_workspace.testacc.id
+					  project_key = bitbucket_project.testacc.key
+					  name        = "%s"
+					}
+	
+					resource "bitbucket_branch_restriction" "testacc" {
+					  workspace  = data.bitbucket_workspace.testacc.id
+					  repository = bitbucket_repository.testacc.name
+					  pattern    = "%s"
+					  kind       = "%s"
+					}
+
+					data "bitbucket_branch_restriction" "testacc" {
+					  workspace  = data.bitbucket_workspace.testacc.id
+					  repository = bitbucket_repository.testacc.name
+					  id         = bitbucket_branch_restriction.testacc.id
+					}`, workspaceSlug, projectName, projectKey, repoName, branchRestrictionPattern, branchRestrictionKind),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.bitbucket_branch_restriction.testacc", "workspace", workspaceSlug),
+					resource.TestCheckResourceAttr("data.bitbucket_branch_restriction.testacc", "repository", repoName),
+					resource.TestCheckResourceAttr("data.bitbucket_branch_restriction.testacc", "pattern", branchRestrictionPattern),
+					resource.TestCheckResourceAttr("data.bitbucket_branch_restriction.testacc", "kind", branchRestrictionKind),
+					resource.TestCheckResourceAttr("data.bitbucket_branch_restriction.testacc", "value", "0"),
+
+					resource.TestCheckNoResourceAttr("data.bitbucket_branch_restriction.testacc", "users"),
+					resource.TestCheckNoResourceAttr("data.bitbucket_branch_restriction.testacc", "groups"),
+
+					resource.TestCheckResourceAttrSet("data.bitbucket_branch_restriction.testacc", "id"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccBitbucketBranchRestrictionDataSource_withKindAndValueCombination(t *testing.T) {
+	workspaceSlug := os.Getenv("BITBUCKET_USERNAME")
+	projectName := "tf-acc-test-" + acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
+	projectKey := strings.ToUpper(acctest.RandStringFromCharSet(3, acctest.CharSetAlpha))
+	repoName := "tf-acc-test-" + acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
+	branchRestrictionPattern := "master"
+	branchRestrictionKind := "require_passing_builds_to_merge"
+	branchRestrictionValue := 3
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+					data "bitbucket_workspace" "testacc" {
+						id = "%s"
+					}
+	
+					resource "bitbucket_project" "testacc" {
+					  workspace = data.bitbucket_workspace.testacc.id
+					  name      = "%s"
+					  key       = "%s"
+					}
+	
+					resource "bitbucket_repository" "testacc" {
+					  workspace   = data.bitbucket_workspace.testacc.id
+					  project_key = bitbucket_project.testacc.key
+					  name        = "%s"
+					}
+	
+					resource "bitbucket_branch_restriction" "testacc" {
+					  workspace  = data.bitbucket_workspace.testacc.id
+					  repository = bitbucket_repository.testacc.name
+					  pattern    = "%s"
+					  kind       = "%s"
+                      value      = %d
+					}
+
+					data "bitbucket_branch_restriction" "testacc" {
+					  workspace  = data.bitbucket_workspace.testacc.id
+					  repository = bitbucket_repository.testacc.name
+					  id         = bitbucket_branch_restriction.testacc.id
+					}`, workspaceSlug, projectName, projectKey, repoName, branchRestrictionPattern, branchRestrictionKind, branchRestrictionValue),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.bitbucket_branch_restriction.testacc", "workspace", workspaceSlug),
+					resource.TestCheckResourceAttr("data.bitbucket_branch_restriction.testacc", "repository", repoName),
+					resource.TestCheckResourceAttr("data.bitbucket_branch_restriction.testacc", "pattern", branchRestrictionPattern),
+					resource.TestCheckResourceAttr("data.bitbucket_branch_restriction.testacc", "kind", branchRestrictionKind),
+					resource.TestCheckResourceAttr("data.bitbucket_branch_restriction.testacc", "value", strconv.Itoa(branchRestrictionValue)),
+
+					resource.TestCheckNoResourceAttr("data.bitbucket_branch_restriction.testacc", "users"),
+					resource.TestCheckNoResourceAttr("data.bitbucket_branch_restriction.testacc", "groups"),
+
+					resource.TestCheckResourceAttrSet("data.bitbucket_branch_restriction.testacc", "id"),
+				),
+			},
+		},
+	})
+}
