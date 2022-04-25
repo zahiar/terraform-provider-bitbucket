30dc16c9191d8b4b1a1de4a6b43490713460e7f5
Zahiar Ahmed <310030+zahiar@users.noreply.github.com>
2022-04-25 01:32:49 +0100

    GH-67 Add new `bitbucket_user_workspace` data source (#94)
    * GH-67 Add new `bitbucket_user_workspace` data source
    
    This allows for finding users by just their Bitbucket nickname - currently experimental, needs more validation before it can be promoted to stable.
    
    And update `go-bitbucket` module.

---
+++ bitbucket/data_source_bitbucket_user_workspace_test.go
@@ -0,0 +1,40 @@
+package bitbucket
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccBitbucketUserWorkspaceDataSource_basic(t *testing.T) {
+	workspace := os.Getenv("BITBUCKET_USERNAME")
+	user, _ := getCurrentUser()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+					data "bitbucket_workspace" "testacc" {
+						id = "%s"
+					}
+
+					data "bitbucket_user_workspace" "testacc" {
+						nickname  = "%s"
+						workspace = data.bitbucket_workspace.testacc.id
+					}`, workspace, user.Nickname),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.bitbucket_user_workspace.testacc", "id", user.Uuid),
+					resource.TestCheckResourceAttr("data.bitbucket_user_workspace.testacc", "nickname", user.Nickname),
+					resource.TestCheckResourceAttr("data.bitbucket_user_workspace.testacc", "display_name", user.DisplayName),
+					resource.TestCheckResourceAttr("data.bitbucket_user_workspace.testacc", "account_status", user.AccountStatus),
+					resource.TestCheckResourceAttr("data.bitbucket_user_workspace.testacc", "account_id", user.AccountId),
+					resource.TestCheckResourceAttr("data.bitbucket_user_workspace.testacc", "workspace", workspace),
+				),
+			},
+		},
+	})
+}
